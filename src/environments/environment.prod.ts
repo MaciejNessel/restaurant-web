de2d7165c80274fe466243e0795a30e01067e78e
MaciejNessel <maciej.nessel@gmail.com>
2022-02-22 12:35:15 +0100

    added firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDbhcg_TelU8959bFcCmfyarMpSZPAaI1s",
+    authDomain: "restaurantwithauth-2505c.firebaseapp.com",
+    databaseURL: "https://restaurantwithauth-2505c-default-rtdb.firebaseio.com",
+    projectId: "restaurantwithauth-2505c",
+    storageBucket: "restaurantwithauth-2505c.appspot.com",
+    messagingSenderId: "284132437607",
+    appId: "1:284132437607:web:0685d5215150678139aec2",
+    measurementId: "G-FB1QNTJ4E3"
+  }
 };
